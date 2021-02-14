7fbc8ca90da8e7bee12a7c35b18f6ad2cb00a160
Edgar Lopez <edgarlopez633@gmail.com>
2021-02-14 08:42:44 -0800

    added graph, calculated total project time

--- drg_data_reader.py
+++ drg_data_reader.py
@@ -1,11 +1,10 @@
 import json
+import matplotlib.pyplot as plt
 
 def pretty(obj):
     return json.dumps(obj, sort_keys=True, indent=2)
 
 timeline_data = json.load(open("timeline_data.json",'r'))
-
-# print(pretty(timeline_data["data"]))
 
 # FETCH ACTIVITY SET AND CREATE DICTIONARY
 timeline_data_dict = {}
@@ -14,24 +13,21 @@ for activity in timeline_data["activitySet"]["activities"]:
     timeline_data_dict[id] = {"name":timeline_data["activitySet"]["activities"][id]["name"], "duration":[], "total_time":0}
     id += 1
 
-# print(timeline_data_dict)
+total_project_time = 0
 
 # GO THROUGH DATA SET AND ADD DURATION VALUES TO DICTIONARY
+# AND CALCULATE TOTAL TIME SPENT ON PROJECT
 for session in timeline_data["data"]:
     activity_id = 0
+    total_project_time += session["duration"]
     for activity in session["data"]:
-        # print(activity)
-        # print(timeline_data["activitySet"]["activities"][activity_id]["name"])
         timeline_data_dict[activity_id]["duration"].append(activity)
         activity_id += 1
 
-# print(pretty(timeline_data_dict))
-
+# SUBTRACT START TIME FROM END TIME TO FIND TIME SPENT ON EACH ACTIVITY
 for activity in timeline_data_dict:
-    # print(timeline_data_dict[activity])
     for time_period in timeline_data_dict[activity]["duration"]:
         for timestamp in time_period:
-            # print(timestamp[1] - timestamp[0])
             timeline_data_dict[activity]["total_time"] += timestamp[1] - timestamp[0]
 
 # Converting milliseconds to minutes
@@ -39,14 +35,22 @@ for activity in timeline_data_dict:
     timeline_data_dict[activity]["total_time"] = timeline_data_dict[activity]["total_time"] / 60000
 
 print(pretty(timeline_data_dict))
-# ONE ACTIVITY
-# print(pretty(timeline_data_dict[0]["duration"][0]))
 
-# SET OF TWO
-# print(pretty(timeline_data_dict[0]["duration"][0][0]))
+# PLOTTING TIME
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_title('Comparison of time spent on each design activity')
+ax.set_xlabel('Activities')
+ax.set_ylabel('Total time spent in minutes')
+activity_list = []
+time_spent = []
+for activity in timeline_data_dict:
+    activity_list.append(timeline_data_dict[activity]["name"])
+    time_spent.append(timeline_data_dict[activity]["total_time"])
 
-    # print("\nBANANA AND TURTLE\n")
-    # print(pretty(session))
-    # print(pretty(session["data"][1]))
-    # for activity in session["data"]:
-    #     print(activity)
+# adding bar for total time spent
+activity_list.append("total project time")
+time_spent.append(total_project_time/60000)
+
+ax.bar(activity_list,time_spent)
+plt.show()
---
+++ readme.txt
@@ -0,0 +1,7 @@
+# Steps to use this:
+1) Replace time_data.json with your own json export (with the same name)
+2) Export settings:
+   1) Use project time
+   2) Include session metadata
+   3) Raw on/off pairs
+   4) Group by activity
